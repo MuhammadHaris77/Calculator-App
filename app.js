0e90cb996684a850162c8d13254a45ddd4656485
haris qureshi <muhammad_haris>
2024-01-10 22:55:31 +0500

    calculator App Assignment

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+ var a = document.getElementById('getValue');
+  function cal(num){
+        a.value += num 
+
+        }
+
+  function result(){
+      a.value = eval(a.value)
+      
+  }
+  function reset(){
+      a.value = ""
+  }
